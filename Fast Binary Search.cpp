339713e63c640e4eee3650cf96c61e61c17538cd
Chathrinda Ranasinghe <chathurindaranasinghe@gmail.com>
2021-09-30 23:49:10 +0530

    added fast binary search

---
+++ Fast Binary Search.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, A[N];
+ 
+int binary_search(int val)
+{
+    int i, step;
+    for (step = 1; step < N; step <<= 1);
+    for (i = 0; step; step >>= 1)
+        if (i + step < N && A[i + step] <= val)
+           i += step;
+    return i;
+}
